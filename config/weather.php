c83a839b2cf3f4120fec14f13037a4f21e0bf857
Mohamed Shuaau <mohammedshuaau@gmail.com>
2020-09-25 17:35:09 +0500

    Set default API key default value

--- config/weather.php
+++ config/weather.php
@@ -3,7 +3,7 @@
 return [
 
     'base_url' => env('WEATHER_API_BASE_URL', 'http://api.openweathermap.org/data/2.5/'),
-    'api_key' => env('WEATHER_API_KEY', '2c45e8649388c571196bfcab69db107f'),
+    'api_key' => env('WEATHER_API_KEY', 'yourapikey'),
 
     'lang' => env('WEATHER_API_LANG', 'en'),
 
